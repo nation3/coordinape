e3a405076339a6a372ef478d8cc78af81de644b6
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-06-06 12:10:05 +0800

    chore: convert coordinape payout csvs (#1)
    Get the guild's budget

---
+++ GuildBudget.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GuildBudget = void 0;
+/**
+ * The amount of $NATION budgeted for each guild.
+ */
+var GuildBudget;
+(function (GuildBudget) {
+    GuildBudget[GuildBudget["CommunityGuild"] = 0.1] = "CommunityGuild";
+    GuildBudget[GuildBudget["BrandDesignGuild"] = 0.1] = "BrandDesignGuild";
+    GuildBudget[GuildBudget["EventsGuild"] = 0.1] = "EventsGuild";
+    GuildBudget[GuildBudget["RewardsGuild"] = 0.1] = "RewardsGuild";
+    GuildBudget[GuildBudget["ResearchGuild"] = 0.1] = "ResearchGuild";
+    GuildBudget[GuildBudget["DevelopersGuild"] = 0.1] = "DevelopersGuild";
+    GuildBudget[GuildBudget["MetaGuild"] = 0.1] = "MetaGuild";
+    GuildBudget[GuildBudget["GrowthGuild"] = 0.1] = "GrowthGuild";
+})(GuildBudget = exports.GuildBudget || (exports.GuildBudget = {}));
---
+++ GuildBudget.ts
@@ -0,0 +1,13 @@
+/**
+ * The amount of $NATION budgeted for each guild.
+ */
+export enum GuildBudget {
+    CommunityGuild = 0.10,
+    BrandDesignGuild = 0.10,
+    EventsGuild = 0.10,
+    RewardsGuild = 0.10,
+    ResearchGuild = 0.10,
+    DevelopersGuild = 0.10,
+    MetaGuild = 0.10,
+    GrowthGuild = 0.10
+}
